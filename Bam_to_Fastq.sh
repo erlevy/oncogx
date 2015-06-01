585798f6a8856f16a94ab2ea3edce298a52a786c
Eric <edlevy@ucsd.edu>
2015-06-01 11:09:21 -0700

    First commit

---
+++ Bam_to_Fastq.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Arg 1: bam file
+# Arg 2: bed file
+# Arg3: output directory
+# Arg4: output directory intermedia files
+# sample usage:
+#   ./Bam_to_Fastq-fromTCGA-Pairend.sh ../NomenclatureTRBgenes/BAM_files/Bam_Files_TCGA/TCGA-B6-A0IJ-01A-11W-A050-09_IlluminaGA-DNASeq_exome_1.sorted.bam  /mnt/oncogxA/Valentina/NomenclatureTRBgenes/SureSelect/Bait-exome_SureSelect/TRA-B-D-G/Target-genesTR.ABDG-GenomicReg.bed outdirectory1 outdirectory2
+region_file="$2" #"/mnt/oncogxA/Valentina/NomenclatureTRBgenes/SureSelect/Bait-exome_SureSelect/TRA-B-D-G/Target-genesTR.ABDG-GenomicReg.bed"
+name=`basename $1 ".bam"`
+bam_inpath=`dirname $1`
+bam_outpath=/scratch
+outdir=$3
+outdir2=$4
+
+#cp $1 /scratch
+#cp $1.bai /scratch
+#samtools index $bam_outpath/$name.bam
+
+touch $bam_outpath/$name.summary.txt
+
+# getting the flagstat from the raw bam file 
+echo 'FLAGSTAT:' >> $bam_outpath/$name.summary.txt
+samtools flagstat $bam_inpath/$name.bam >> $bam_outpath/$name.summary.txt 
+
+# extracting overlapping+unmapped TCR reads
+#mv $bam_outpath/$name.bam $bam_outpath/$name.TCRreg.bam
+#samtools view -b -L $region_file $bam_outpath/$name.bam > $bam_outpath/$name.TCRreg.bam
+samtools view -b -F 4 $bam_inpath/$name.bam 7:38295938-38407399 7:142000817-142510993 9:33618203-33662661 14:22090036-23014042 > $bam_outpath/$name.TCR.bam
+wait
+samtools view -b -f 4 $bam_inpath/$name.bam > $bam_outpath/$name.unmapped.bam
+wait
+samtools index $bam_outpath/$name.TCR.bam
+wait
+samtools index $bam_outpath/$name.unmapped.bam
+wait
+samtools merge $bam_outpath/$name.TCRreg.bam $bam_outpath/$name.TCR.bam $bam_outpath/$name.unmapped.bam
+wait
+echo 'Overlap-Unmapped-count:' >> $bam_outpath/$name.summary.txt
+samtools view -c $bam_outpath/$name.TCRreg.bam >> $bam_outpath/$name.summary.txt # unmapped-overlapping-count
+wait
+samtools sort -n  $bam_outpath/$name.TCRreg.bam $bam_outpath/$name.TCRreg.sorted
+wait
+#bedtools bamtofastq -i $bam_outpath/$name.TCRreg.sorted.bam -fq $bam_outpath/$name.TCRreg.fastq
+#wait
+#bgzip $bam_outpath/$name.TCRreg.fastq
+#wait
+
+
+# move back
+#mv $bam_outpath/$name.TCRreg.fastq.gz $outdir
+mv $bam_outpath/$name.summary.txt $outdir
+mv $bam_outpath/$name.TCRreg.sorted.bam $outdir
+wait
+
+# clean up
+rm $bam_outpath/$name*
+
+#rm $bam_outpath/$name.TCRreg.bam.bai
+#rm $bam_outpath/$name.unmapped.bam
+#rm $bam_outpath/$name.unmapped.bam.bai
+#rm $bam_outpath/$name.TCRreg.merged.bam
+#rm $bam_outpath/$name.TCRreg.merged.bam.bai
+#rm $bam_outpath/$name.TCRreg.merged.sorted.bam
