25ac2c000b8c8da522e91c060d0ed89b60973b93
Eric <edlevy@ucsd.edu>
2015-06-01 15:06:38 -0700

    Full bwa mem command

--- exome_alignment.sh
+++ exome_alignment.sh
@@ -1,1 +1,5 @@
-/mnt/idash/Genomics/bin/bwa mem
+input='/scratch/immunoseq/fastq'
+ref='/mnt/idash/Genomics/data_resources/references-and-indexes/hg19/bwa_v7'
+output='/scratch/immunoseq/aligned'
+
+/mnt/idash/Genomics/bin/bwa mem -M -t 26 ${ref}/hg19_lite.fa ${input}/merged_R1.fastq ${input}/merged_R2.fastq > ${output}/aln.sam
