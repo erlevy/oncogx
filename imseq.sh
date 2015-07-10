9eb853179679ed2bde2f25fe5ceae2243b2f92c6
Eric <edlevy@ucsd.edu>
2015-07-10 13:53:31 -0700

    fixing imseq again again

--- imseq.sh
+++ imseq.sh
@@ -9,12 +9,13 @@
 workdir=$1
 CNT=1
 outdir="/scratch/data/bamslicer/imseq"
+datadir="/scratch/data/bamslicer/data"
 for file in $workdir/*fastq.gz; do
 
   name=`basename $file ".TCRreg.fastq.gz"`
   gzip -d $file
   wait
-  ./imseq -ref Homo.Sapiens.TRB.fa -o $outdir/$name.tsv $file &
+  ./imseq -ref Homo.Sapiens.TRB.fa -o $outdir/$name.tsv $datadir/$name.TCRreg.fastq &
   
   if [ $(($CNT % 26)) -eq 0 ]; then
     echo "##DOING 26##"
